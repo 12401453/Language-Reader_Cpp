05a74fd977edc0889e9e73ab75cd55055a81b5ac
12401453 <12401453@protonmail.com>
2023-07-07 18:22:31 +0100

    sozdik.kz

--- Linux/HTML_DOCS/sozdik_lookup.js
+++ Linux/HTML_DOCS/sozdik_lookup.js
@@ -1,4 +1,5 @@
 let sozdik_result = Object.create(null);
+
 const dictLookupSozdik = (word) => {
     let send_data = "url=https://sozdik.kz/translate/kk/ru/"+encodeURIComponent(word)+"/";
     const httpRequest = (method, url) => {
@@ -9,6 +10,7 @@ const dictLookupSozdik = (word) => {
         xhttp.onreadystatechange = () => {
             if (xhttp.readyState == 4) {
             sozdik_result = JSON.parse(xhttp.responseText);
+            //scrapeSozdik(sozdik_result);
             console.log(sozdik_result);
             }
         };
@@ -16,3 +18,43 @@ const dictLookupSozdik = (word) => {
     };
     httpRequest("POST", "curl_lookup.php");
 };
+
+let sozdik_parsed_result = Object.create(null);
+const scrapeSozdik = (sozdik_result) => {
+    const parser = new DOMParser();
+    let sozdikHTML = parser.parseFromString(sozdik_result.data.translation, 'text/html');
+
+    sozdikHTML.body.querySelectorAll('body > details').forEach( (details_elem, i) => {
+
+        let summary_text = "";
+        details_elem.querySelectorAll('body > details')[1].querySelector('summary').childNodes.forEach(childNode => {
+            if(childNode.nodeName == "a") {
+                summary_text += childNode.textContent;
+            }
+            else summary_text += childNode.innerHTML;
+        });
+        console.log(summary_text);
+        
+        sozdik_parsed_result[i] = summary_text;
+
+        /*let second_tier_details = */details_elem.querySelectorAll('details > details').forEach(details_elem_2 => {
+
+        });
+        
+    });
+
+    /*let p_elements = sozdikHTML.getElementsByTagName("p");
+    let p_length = p_elements.length;
+    let prev_parent = 0;
+    let first_tier = 0;
+    sozdik_parsed_result[first_tier] = {};
+    for(let i = 0; i < p_length; i++) {
+        current_parent = p_elements[i].parentElement;
+        if(prev_parent != p_elements[i].parentElement) {
+            first_tier++;
+            sozdik_parsed_result[first_tier] = {};
+        }
+        sozdik_parsed_result[f]
+        prev_parent = 
+    } */
+}
