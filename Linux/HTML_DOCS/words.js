791be7e4a05a7713d8d3306e24e85e8e932c7efa
12401453 <12401453@protonmail.com>
2023-09-08 15:05:16 +0100

    ..

--- Linux/HTML_DOCS/words.js
+++ Linux/HTML_DOCS/words.js
@@ -1,3 +1,10 @@
+document.getElementById("pos_selectors").addEventListener('click', (event) => { 
+  if(event.target.classList.contains("pos_sel")) {
+    event.target.classList.toggle("pos_selected");
+  }
+}
+);
+
 const setSearchboxSize = function (event) {
   let searchbox = event.target;
   let hidden_searchbox = document.getElementById('hidden_searchbox');
@@ -11,3 +18,16 @@ const setSearchboxSize = function (event) {
 };
 document.getElementById("lemma_searchbox").addEventListener('input', setSearchboxSize);
 document.getElementById("meaning_searchbox").addEventListener('input', setSearchboxSize);
+
+
+
+const addRow = () => {
+    document.getElementById("words_body").appendChild(document.createRange().createContextualFragment('<div class="words_row"><div class="word_cell_left"><div class="pos_colour left adj"></div><div class="word_cell_lemma">hinanden</div><div class="pos_colour right adj"></div></div><div class="word_cell_right"></div></div>')); 
+};
+
+const addSubRow = (main_row_no) => {
+    if(main_row_no < 1) return;
+    let words_rows = document.getElementById("words_body").getElementsByClassName("words_row");
+    if(words_rows.length < main_row_no) return;
+    words_rows[main_row_no - 1].querySelector(".word_cell_right").appendChild(document.createRange().createContextualFragment(`<div class="word_cell_meaning_strip"><div class="meaning_no_box"><div class="meaning_no" title="Meaning number">${main_row_no}</div></div><div class="meaning_text">right side dummy meaning hahahaha you gormless wazzok</div></div>`));
+};
